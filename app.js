ef3e158ef018c14f44956e69e5457f1d8eade4d4
Gromzi <mamgier@gmail.com>
2022-10-31 18:08:25 +0100

    Create textboxes and add data atributes to elements. Create app.js and select appropriate elements

---
+++ app.js
@@ -0,0 +1,4 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const clearButton = document.querySelectorAll('[data-clear]')
+const clearAllButton = document.querySelectorAll('[data-clear-all]')
