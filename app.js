eae0f14aac497012eded4e48995e531e1259e8c0
Gromzi <mamgier@gmail.com>
2022-11-07 22:36:54 +0100

    Finish the back-end

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const addButton = document.querySelector('[data-add]');
 const subtractButton = document.querySelector('[data-subtract]');
 const multiplyButton = document.querySelector('[data-multiply]');
 const divideButton = document.querySelector('[data-divide]');
+const pqButton = document.querySelector('[data-pq]');
 const clearButton = document.querySelector('[data-clear]');
 const clearAllButton = document.querySelector('[data-clear-all]');
 const panel4 = document.getElementById("panel4-textbox");
@@ -17,6 +18,8 @@ const panel2 = document.getElementById("panel2-textbox");
 const panel1 = document.getElementById("panel1-textbox");
 
 const panels =  Array(panel1, panel2, panel3, panel4);
+
+let panel1ShouldUnshift = false;
 
 
 function isEmptyArrayIndex(array, index) {
@@ -27,7 +30,14 @@ function refreshPanels(stack){
         isEmptyArrayIndex(stack,i) ? panels[i].innerHTML = "" : panels[i].innerHTML = stack[i];
     }
 }
-
+function isPrime(num) {
+    if(num < 2) return false;
+    for (var i = 2; i < num; i++) {
+        if(num % i == 0)
+            return false;
+    }
+    return true;
+}
 
 
 let stack = Array();
@@ -61,20 +71,53 @@ stack.swap = function () {
     refreshPanels(this);
 }
 stack.power = function () {
+    if(Math.pow(this[0],this[1])>Number.MAX_SAFE_INTEGER) {
+        panel1.innerHTML = "Za duża liczba";
+        panel1ShouldUnshift = true;
+        return;
+    }
+
     let x = this.shift();
     let y = this.shift();
     this.unshift(Math.pow(x,y));
 }
 stack.frac = function () {
-    // console.log(generatePrimeFactors(this[0]));
-    // var content = "$${{ -b \\pm \\sqrt {{b^2} - 4ac}} \\over {2a}}$$";
-    // // panel1.innerHTML = content
-    // console.log("alala");
-    // MathJax.Hub.queue(["Text",panel1,content]);
+    let primeFactors = generatePrimeFactors(this[0]);
 
-    let panel1 = document.getElementById("panel1-textbox"); 
-    panel1.innerHTML="\\({{a}_{x}}=\\frac{\\,d^2 z}{\\,dt^2}\\)";
+    let panel1 = document.getElementById("panel1-textbox");
+    let text = `\\({${this[0]}}=`;
+    for(let i=0; i<primeFactors.length; i++){
+        if(i<primeFactors.length-1){
+            text += `{\\,${primeFactors[i][0]}^${primeFactors[i][1]} \\cdot}`;
+        }else{
+            text += `{\\,${primeFactors[i][0]}^${primeFactors[i][1]} }`;
+        }
+
+    } 
+    text+= `\\)`;
+    
+    panel1.innerHTML = text;
     MathJax.typeset();
+}
+stack.pq = function () {
+    let arg = this[0];
+
+    if( arg <= 4 | arg%2 != 0)
+    {
+        panel1.innerHTML = 'Działa na parzystych > 4';
+    }
+    else{
+      let x = arg
+      for(let i=3; i<=(x/2); i++)
+      {
+        if(isPrime(x-i) & isPrime(i))
+        {
+            panel1.innerHTML = `\\( ${x.toString()} = ${i.toString()} + ${(x-i).toString()} \\)`;
+            MathJax.typeset();
+        }
+      }
+    }
+    panel1ShouldUnshift = true;
 }
 stack.mod =  function () {
     let x = this.shift();
@@ -82,16 +125,32 @@ stack.mod =  function () {
     this.unshift(y%x);
 }
 stack.add = function () {
+    if(this[0]+this[1]>Number.MAX_SAFE_INTEGER) {
+        panel1.innerHTML = "Za duża liczba";
+        panel1ShouldUnshift = true;
+        return;
+    }
+
     let x = this.shift();
     let y = this.shift();
     this.unshift(y+x);
 }
 stack.subtract = function () {
+    if(this[1]-this[0]<0) {
+        panel1.innerHTML = "Liczba mniejsza niż 0";
+        panel1ShouldUnshift = true;
+        return;
+    }
     let x = this.shift();
     let y = this.shift();
     this.unshift(y-x);
 }
 stack.multiply = function () {
+    if(this[1]*this[0]>Number.MAX_SAFE_INTEGER) {
+        panel1.innerHTML = "Za duża liczba";
+        panel1ShouldUnshift = true;
+        return;
+    }
     let x = this.shift();
     let y = this.shift();
     this.unshift(y*x);
@@ -110,6 +169,12 @@ stack.divide = function () {
 
 
 function appendNumber(number) {
+    if(panel1ShouldUnshift) {
+        stack.unshift("");
+        // panel1.innerHTML = "" ;
+        panel1ShouldUnshift = false;
+    }
+
     panel1.innerHTML += number
 
     stack[0] = Number(panel1.innerHTML);
@@ -140,6 +205,8 @@ popButton.addEventListener('click', () => {
 // Enter button
 enterButton.addEventListener('click', () => {
     if (panel1.innerHTML !== "") {
+        if (panel1ShouldUnshift) return;
+
         stack.unshift("");
     }
 })
@@ -160,10 +227,8 @@ powerButton.addEventListener('click', () => {
 
 // Frac Button
 fracButton.addEventListener('click', () => {
-    // if (panel1.innerHTML !== "") {
-    //     stack.frac();
-    // }
     stack.frac();
+    panel1ShouldUnshift = true;
 })
 
 // Mod Button
@@ -198,5 +263,12 @@ multiplyButton.addEventListener('click', () => {
 divideButton.addEventListener('click', () => {
     if (panel1.innerHTML !== "" && panel2.innerHTML !== "") {
         stack.divide();
+    }
+})
+
+// Pq Button
+pqButton.addEventListener('click', () => {
+    if (panel1.innerHTML !== "") {
+        stack.pq();
     }
 })
